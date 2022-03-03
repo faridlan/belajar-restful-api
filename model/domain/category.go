b58179b11025cf5861c7f7db430ac579528d3739
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2022-03-04 00:24:14 +0700

    Modified model to add tags json and validate in struct

--- model/domain/category.go
+++ model/domain/category.go
@@ -1,6 +1,6 @@
 package domain
 
 type Category struct {
-	Id   int
-	Name string
+	Id   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
--- model/web/category_response.go
+++ model/web/category_response.go
@@ -1,6 +1,6 @@
 package web
 
 type CategoryResponse struct {
-	Id   int
-	Name string
+	Id   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
